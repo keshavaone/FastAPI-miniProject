8007f5f4b5d758f432f80a08ba26c3db7896e419
Keshava <keshava_one@outlook.com>
2024-06-23 16:28:04 +0530

    Learnt new Concept on FastAPI-QUERY

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Query
+from typing import Annotated
 app = FastAPI()
 
 @app.get("/")
-def hello():
-    return {"FirstInitiate":"Hello World"}
+def read_data():
+    return {"status": "OK"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id:int,
+              q:Annotated[list[str]|None,Query(max_length=30)]=None,
+              short: bool = False):
+    item = {"item_id":item_id}
+    if q:
+        item.update({"q":q})
+    if short:
+        item.update(
+            {"description": "This is a long description"}
+        )
+    return item
